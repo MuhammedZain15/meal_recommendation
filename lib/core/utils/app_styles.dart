d1e448d8d4efe8365bd313e5131a67f81d715577
Maheresio <zamalek.545r5@gmail.com>
2025-03-10 19:26:50 +0200

    update style

--- lib/core/utils/app_styles.dart
+++ lib/core/utils/app_styles.dart
@@ -1,21 +1,21 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 
 abstract class AppStyles {
   static TextStyle textStyle13(BuildContext context) => TextStyle(
-        fontSize: 13
-        ,       color: Colors.grey[600],
+        fontSize: 13.sp        ,       color: Colors.grey[600],
         fontWeight: FontWeight.bold,
       );
 
   static TextStyle textStyle30(BuildContext context) => TextStyle(
-        fontSize: 30,
+        fontSize: 30.sp,
         color: Colors.white,
         fontWeight: FontWeight.bold,
       );
   static TextStyle textStyle40(BuildContext context) => TextStyle(
-        fontSize: 40,
+        fontSize: 40.sp,
         color: Colors.grey[600],
         fontWeight: FontWeight.bold,
       );
