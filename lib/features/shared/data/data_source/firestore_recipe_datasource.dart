6975c512f4db21f659c1e69c4b7babf0b17b296d
Maheresio <zamalek.545r5@gmail.com>
2025-03-10 18:03:22 +0200

    update structure

--- lib/features/shared/data/data_source/firestore_recipe_datasource.dart
+++
@@ -1,5 +0,0 @@
-import 'package:meal_recommendation/features/shared/data/data_source/firebase_auth_datasource.dart';
-
-class FirestoreRecipeDatasource {}
-
-class FirebaseAuthDatasourceImpl implements FirebaseAuthDataSource {}
--- lib/features/shared/domain/usecase/add_favorite_usecase.dart
+++
@@ -1,4 +0,0 @@
-class AddFavoriteUsecase {
-
-  void execute() {}
-}
