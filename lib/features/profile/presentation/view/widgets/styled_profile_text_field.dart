9b1900a8123883287da6047e91a968b08fb4b0ef
Maheresio <zamalek.545r5@gmail.com>
2025-03-14 15:29:09 +0200

    refactor styled profile text field to accept external controller and obscure text option

--- lib/features/profile/presentation/view/widgets/styled_profile_text_field.dart
+++ lib/features/profile/presentation/view/widgets/styled_profile_text_field.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
 
 class StyledProfileTextField extends StatelessWidget {
-  const StyledProfileTextField({super.key});
+  const StyledProfileTextField({
+    super.key,
+    required this.controller,
+    this.obsecureText = false,
+  });
 
+  final TextEditingController controller;
+  final bool obsecureText;
   @override
   Widget build(BuildContext context) {
     return TextField(
+      obscureText: obsecureText,
       decoration: InputDecoration(
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(8.0),
           borderSide: BorderSide(color: Colors.black, width: 1.0),
         ),
       ),
-      controller: TextEditingController(text: 'Initial Text'),
+      controller: controller,
     );
   }
 }
