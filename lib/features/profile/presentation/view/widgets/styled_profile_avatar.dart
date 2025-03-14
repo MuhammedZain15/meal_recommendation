152a3a7a40731824f28ea373937f06e4e7eb5778
Maheresio <zamalek.545r5@gmail.com>
2025-03-14 14:37:38 +0200

    create style profile avatar widget

---
+++ lib/features/profile/presentation/view/widgets/styled_profile_avatar.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import '../../../../../core/utils/app_colors.dart';
+
+class StyledProfileAvatar extends StatelessWidget {
+  const StyledProfileAvatar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          height: 100,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: AppColors.kGrey,
+          ),
+        ),
+        Positioned(
+          top: 20,
+          right: 0,
+          left: 70,
+          bottom: 0,
+          child: Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 35,
+              width: 35,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: AppColors.kPrimary,
+              ),
+              child: Icon(FontAwesomeIcons.pen, color: Colors.white, size: 16),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
