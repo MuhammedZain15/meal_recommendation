a1e0f17872e5b489c70bd27d6320abc6262f3c40
Maheresio <zamalek.545r5@gmail.com>
2025-03-14 14:38:21 +0200

    add style app bar and style profile avatar to profile view body

--- lib/features/profile/presentation/view/widgets/profile_view_body.dart
+++ lib/features/profile/presentation/view/widgets/profile_view_body.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:meal_recommendation/core/components/styled_app_bar.dart';
+
+import 'styled_profile_avatar.dart';
 
 class ProfileViewBody extends StatelessWidget {
   const ProfileViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        StyledAppBar(),
+        SizedBox(height: 12),
+
+        StyledProfileAvatar(),
+      ],
+    );
   }
 }
+
