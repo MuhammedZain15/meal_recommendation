03cfd844fa018f0e2bd75428652ef79f00bf0859
Maheresio <zamalek.545r5@gmail.com>
2025-03-14 15:29:21 +0200

    refactor ProfileFormSection to accept external controllers for text fields

--- lib/features/profile/presentation/view/widgets/profile_form_section.dart
+++ lib/features/profile/presentation/view/widgets/profile_form_section.dart
@@ -3,7 +3,18 @@ import 'package:flutter/material.dart';
 import 'styled_profile_text_field.dart';
 
 class ProfileFormSection extends StatelessWidget {
-  const ProfileFormSection({super.key});
+  const ProfileFormSection({
+    super.key,
+    required this.nameController,
+    required this.emailController,
+    required this.phoneController,
+    required this.passwordController,
+  });
+
+  final TextEditingController nameController;
+  final TextEditingController emailController;
+  final TextEditingController phoneController;
+  final TextEditingController passwordController;
 
   @override
   Widget build(BuildContext context) {
@@ -12,13 +23,12 @@ class ProfileFormSection extends StatelessWidget {
       child: Column(
         spacing: 20,
         children: [
-          StyledProfileTextField(),
-          StyledProfileTextField(),
-          StyledProfileTextField(),
-          StyledProfileTextField(),
+          StyledProfileTextField(controller: nameController),
+          StyledProfileTextField(controller: emailController),
+          StyledProfileTextField(controller: phoneController),
+          StyledProfileTextField(controller: passwordController),
         ],
       ),
     );
   }
 }
-
