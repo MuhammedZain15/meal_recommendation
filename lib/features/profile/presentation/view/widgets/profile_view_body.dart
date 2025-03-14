3a7a70372a5ee1d6f9287369ab23c24208892439
Maheresio <zamalek.545r5@gmail.com>
2025-03-14 15:29:33 +0200

    refactor ProfileViewBody to use stateful widget and manage text field controllers

--- lib/features/profile/presentation/view/widgets/profile_view_body.dart
+++ lib/features/profile/presentation/view/widgets/profile_view_body.dart
@@ -1,21 +1,69 @@
 import 'package:flutter/material.dart';
 import 'package:meal_recommendation/core/components/styled_app_bar.dart';
+import 'package:meal_recommendation/core/utils/app_strings.dart';
 
+import '../../../../../core/components/styled_elevated_button.dart';
+import 'profile_form_section.dart';
 import 'styled_profile_avatar.dart';
 
-class ProfileViewBody extends StatelessWidget {
+class ProfileViewBody extends StatefulWidget {
   const ProfileViewBody({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        StyledAppBar(),
-        SizedBox(height: 12),
+  State<ProfileViewBody> createState() => _ProfileViewBodyState();
+}
 
-        StyledProfileAvatar(),
-      ],
+class _ProfileViewBodyState extends State<ProfileViewBody> {
+  late final TextEditingController _nameController;
+  late final TextEditingController _emailController;
+  late final TextEditingController _phoneController;
+  late final TextEditingController _passwordController;
+
+  @override
+  void initState() {
+    super.initState();
+    _nameController = TextEditingController(text: 'John Doe');
+    _emailController = TextEditingController(text: 'John Doe');
+    _phoneController = TextEditingController(text: 'John Doe');
+    _passwordController = TextEditingController(text: 'John Doe');
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _emailController.dispose();
+    _phoneController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 16),
+      child: Column(
+        children: [
+          StyledAppBar(),
+          SizedBox(height: 12),
+
+          StyledProfileAvatar(),
+          SizedBox(height: 30),
+          ProfileFormSection(
+            nameController: _nameController,
+            emailController: _emailController,
+            phoneController: _phoneController,
+            passwordController: _passwordController,
+          ),
+          SizedBox(height: 40),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12.0),
+            child: StyledElevatedButton(
+              text: AppStrings.kSave,
+              onPressed: () {},
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
-
