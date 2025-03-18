a5681d7ffbf61bcd28ecd1d30ced0f7847749827
Maheresio <zamalek.545r5@gmail.com>
2025-03-18 04:18:54 +0200

    Add new food images and update AppImages utility

--- lib/core/utils/app_images.dart
+++ lib/core/utils/app_images.dart
@@ -8,6 +8,8 @@ class AppImages {
   static const String lock = 'assets/images/Lock.svg';
   static const String food1 = 'assets/images/food1.png';
   static const String food2 = 'assets/images/food2.png';
+  static const String food3 = 'assets/images/food3.png';
+  static const String food4 = 'assets/images/food4.png';
   static const String facebook = 'assets/images/Facebook.png';
   static const String google = 'assets/images/Google.png';
   static const String logo1 = 'assets/images/logo.png';
