c6fd3456fd6f8a14b08be514221925a8cdd0f2b5
Maheresio <zamalek.545r5@gmail.com>
2025-03-18 04:21:08 +0200

    Add HomeViewBody widget with placeholder implementation

---
+++ lib/features/home/presentation/view/widgets/home_view_body.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
