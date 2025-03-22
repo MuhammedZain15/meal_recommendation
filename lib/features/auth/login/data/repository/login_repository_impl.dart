90066c00c32e000df0f72004c0dd56b278fee88d
Maheresio <zamalek.545r5@gmail.com>
2025-03-22 17:50:25 +0200

    Implement login repository with email, Google, and Facebook authentication methods

--- lib/features/auth/login/data/repository/login_repository_impl.dart
+++ lib/features/auth/login/data/repository/login_repository_impl.dart
@@ -1,3 +1,59 @@
-import '../../domain/repository/login_repository.dart';
 
-class LoginRepositoryImpl implements LoginRepository {}
+
+import 'package:firebase_auth/firebase_auth.dart';
+
+import '../../domain/repository/login_repository.dart';
+import '../data_source/login_datasource.dart';
+import '../data_source/secure_storage_data_source.dart';
+
+class LoginRepositoryImpl implements LoginRepository {
+
+
+  final LoginDataSource loginDataSource;
+  final SecureStorageDataSource storageDataSource;
+
+  LoginRepositoryImpl({required this.loginDataSource, required this.storageDataSource});
+
+  @override
+  Future<User?> loginWithEmailAndPassword({required String email,required String password}) async {
+    final user = await loginDataSource.loginWithEmailAndPassword(email: email, password: password);
+    if (user != null) await storageDataSource.saveToken(await user.getIdToken() ?? "");
+    return user;
+  }
+
+  @override
+  Future<User?> loginWithGoogle() async {
+    final user = await loginDataSource.loginWithGoogle();
+    if (user != null) await storageDataSource.saveToken(await user.getIdToken() ?? "");
+    return user;
+  }
+
+  @override
+  Future<User?> loginWithFacebook() async {
+    final user = await loginDataSource.loginWithFacebook();
+    if (user != null) await storageDataSource.saveToken(await user.getIdToken() ?? "");
+    return user;
+  }
+
+  @override
+  Future<void> saveUserToFirestore(User user) async {
+    await loginDataSource.saveUserToFirestore(user);
+  }
+
+  @override
+  Future<void> saveToken(String token) async {
+    await storageDataSource.saveToken(token);
+  }
+
+  @override
+  Future<String?> getToken() async {
+    return await storageDataSource.getToken();
+  }
+
+  @override
+  Future<void> logout() async {
+    await storageDataSource.deleteToken();
+    await FirebaseAuth.instance.signOut();
+  }
+}
+
--- lib/features/auth/login/domain/repository/login_repository.dart
+++ lib/features/auth/login/domain/repository/login_repository.dart
@@ -1,1 +1,11 @@
-class LoginRepository {}
+import 'package:firebase_auth/firebase_auth.dart';
+
+abstract class LoginRepository {
+  Future<User?> loginWithEmailAndPassword({required String email, required String password});
+  Future<User?> loginWithGoogle();
+  Future<User?> loginWithFacebook();
+  Future<void> saveUserToFirestore(User user);
+  Future<void> saveToken(String token);
+  Future<String?> getToken();
+  Future<void> logout();
+}
