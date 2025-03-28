a581aa68dcb2ee2d5f019d2c4d997d5acca19cfb
MuhammedZain15 <muhammedzain1504@gmail.com>
2025-03-28 15:59:35 +0200

    fixed gemini error

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,7 +1,13 @@
 // lib/core/utils/app_router.dart
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:meal_recommendation/features/gemini/data/%20repositories/gemini.dart';
+import 'package:meal_recommendation/features/gemini/data/datasources/gemini_api_service.dart';
+import 'package:meal_recommendation/features/gemini/data/datasources/recipe_api_service.dart';
+import 'package:meal_recommendation/features/gemini/presentation/bloc/recipe_bloc.dart';
+import 'package:meal_recommendation/features/gemini/presentation/pages/recipe_page.dart';
 
 import '../../features/auth/login/domain/usecase/login_with_email_and_password_usecase.dart';
 import '../../features/auth/login/domain/usecase/login_with_google_usecase.dart';
@@ -15,6 +21,7 @@ import '../../features/auth/register/presentation/view/register_view.dart';
 import '../../features/favorite/domain/use_cases/get_favorite_meals_use_case.dart';
 import '../../features/favorite/presentation/controller/favorites_bloc.dart';
 import '../../features/favorite/presentation/pages/favorite_view.dart';
+import '../../features/gemini/domain/usecases/fetch_recipe.dart';
 import '../../features/home/data/models/recipe_model.dart';
 import '../../features/home/presentation/view/home_view.dart';
 import '../../features/home/presentation/view/recipe_details_view.dart';
@@ -40,9 +47,10 @@ abstract class AppRouter {
   static const kProfileView = '/profileView';
   static const kFavoriteView = '/favoriteView';
   static const kRecipeDetailsView = '/recipeDetailsView';
+  static const kGemini = '/gemini';
 
   static final GoRouter router = GoRouter(
-    initialLocation: kSplashView,
+    initialLocation: kLoginView,
     routes: [
       GoRoute(
         path: kSplashView,
@@ -57,9 +65,9 @@ abstract class AppRouter {
           return BlocProvider(
             create:
                 (context) => ProfileBloc(
-                  getUser: sl<GetProfileUseCase>(),
-                  updateUser: sl<UpdateProfileUseCase>(),
-                ),
+              getUser: sl<GetProfileUseCase>(),
+              updateUser: sl<UpdateProfileUseCase>(),
+            ),
             child: const LayoutView(),
           );
         },
@@ -91,10 +99,10 @@ abstract class AppRouter {
           return BlocProvider(
             create:
                 (context) => LoginCubit(
-                  loginWithEmailAndPasswordUseCase:
-                      sl<LoginWithEmailAndPasswordUseCase>(),
-                  loginWithGoogleUseCase: sl<LoginWithGoogleUseCase>(),
-                ),
+              loginWithEmailAndPasswordUseCase:
+              sl<LoginWithEmailAndPasswordUseCase>(),
+              loginWithGoogleUseCase: sl<LoginWithGoogleUseCase>(),
+            ),
             child: const LoginView(),
           );
         },
@@ -106,10 +114,10 @@ abstract class AppRouter {
           return BlocProvider(
             create:
                 (context) => RegisterBloc(
-                  signUpWithEmail: sl<SignUpWithEmailUseCase>(),
-                  signInWithGoogle: sl<SignInWithGoogleUseCase>(),
-                  signInWithFacebook: sl<SignInWithFacebookUseCase>(),
-                ),
+              signUpWithEmail: sl<SignUpWithEmailUseCase>(),
+              signInWithGoogle: sl<SignInWithGoogleUseCase>(),
+              signInWithFacebook: sl<SignInWithFacebookUseCase>(),
+            ),
             child: const RegisterView(),
           );
         },
@@ -121,7 +129,7 @@ abstract class AppRouter {
           return BlocProvider(
             create:
                 (context) =>
-                    FavoritesCubit(sl<GetFavoriteMeals>())..fetchFavorites(),
+            FavoritesCubit(sl<GetFavoriteMeals>())..fetchFavorites(),
             child: const FavoriteView(),
           );
         },
@@ -136,6 +144,24 @@ abstract class AppRouter {
         path: kSeeAllView,
         builder: (context, state) {
           return const SeeAllView();
+        },
+      ),
+      GoRoute(
+        path: kGemini,
+        builder: (context, state) {
+          return BlocProvider(
+            create:
+                (context) => RecipeBloc(
+              fetchRecipeUseCase: FetchRecipeUseCase(
+                repository: GeminiRepositoryImpl(
+                  geminiApi: GeminiApiService(),
+                  recipeApi: RecipeApiService(),
+                  firestore: FirebaseFirestore.instance,
+                ),
+              ),
+            ),
+            child: const RecipePage(),
+          );
         },
       ),
     ],
