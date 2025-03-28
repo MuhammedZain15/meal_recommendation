2c4e00aab56c6d71761b51d74f91b64bf3117731
Maheresio <zamalek.545r5@gmail.com>
2025-03-28 08:06:01 +0200

    Refactor FirebaseAuthServiceImpl to support Google sign-in and user data saving; update method names and dependencies

--- lib/features/auth/shared/services/firebase_auth_service_impl.dart
+++ lib/features/auth/shared/services/firebase_auth_service_impl.dart
@@ -1,43 +1,63 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 import '../../../../core/services/firebase_auth_service.dart';
 
-
 class FirebaseAuthServiceImpl implements FirebaseAuthService {
-  final FirebaseAuth firebaseAuth;
-  const FirebaseAuthServiceImpl(this.firebaseAuth);
+  final FirebaseAuth auth;
+  final FirebaseFirestore firestore;
+  const FirebaseAuthServiceImpl({required this.auth, required this.firestore});
 
   @override
-  Future<void> loginWithEmailAndPassword({
+  Future<User?> signInWithEmailAndPassword({
     required String email,
     required String password,
   }) async {
-    await firebaseAuth.signInWithEmailAndPassword(
+    final credential = await auth.signInWithEmailAndPassword(
       email: email,
       password: password,
     );
+    return credential.user;
   }
 
   @override
-  Future<void> registerWithEmailAndPassword({
+  Future<User?> signInWithGoogle() async {
+    final googleUser = await GoogleSignIn().signIn();
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+    return (await auth.signInWithCredential(credential)).user;
+  }
+
+  @override
+  Future<User?> signUpWithEmailAndPassword({
     required String email,
     required String password,
   }) async {
-    await firebaseAuth.createUserWithEmailAndPassword(
+    final credential = await auth.createUserWithEmailAndPassword(
       email: email,
       password: password,
     );
+    return credential.user;
   }
 
   @override
-  Stream<User?> authStateChanges() =>firebaseAuth.authStateChanges();
-  
+  Stream<User?> authStateChanges() => auth.authStateChanges();
 
   @override
-  User? get currentUser => firebaseAuth.currentUser;
+  User? get currentUser => auth.currentUser;
 
   @override
   Future<void> logOut() async {
-    await firebaseAuth.signOut();
+    await auth.signOut();
+  }
+
+  @override
+  Future<void> saveUserData(String uid, Map<String, dynamic> data) async {
+    await firestore.collection('users').doc(uid).set(data);
   }
 }
